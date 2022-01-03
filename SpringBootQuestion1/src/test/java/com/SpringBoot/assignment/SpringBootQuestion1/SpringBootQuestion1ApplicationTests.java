69dfe16d23566fbf206b8257ab8c9136206d7347
rugawade <rugawade@LIN25008032.corp.capgemini.com>
2022-01-03 18:47:43 +0530

    First Question

---
+++ SpringBootQuestion1/src/test/java/com/SpringBoot/assignment/SpringBootQuestion1/SpringBootQuestion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.assignment.SpringBootQuestion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuestion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
